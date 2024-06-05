a22c4cd8a663ff950d4324f3b850e05487f7e231
Ian Njari <iannjari@gmail.com>
2024-06-05 09:06:31 +0300

    add internal error pages

--- handler/auth_handler.go
+++ handler/auth_handler.go
@@ -23,39 +23,71 @@ func Hello(c *fiber.Ctx) error {
 func Login(c *fiber.Ctx) error {
 	db, err := gorm.Open(postgres.Open(os.Getenv("DATABASE_URL")), &gorm.Config{})
 	if err != nil {
-		panic("unable to open db: " + err.Error())
+		return c.Render("internal_error2", fiber.Map{
+			"ErrorCode": "ERR1000",
+		})
 	}
 	authRequest := new(model.AuthRequest)
 	if err := c.QueryParser(authRequest); err != nil {
-		return c.Status(400).JSON(fiber.Map{"error": "invalid request"})
+		return c.Render("internal_error2", fiber.Map{
+			"ErrorCode": "ERR1001",
+		})
 	}
 
 	if authRequest.ResponseType != "code" {
-		return c.Status(400).JSON(fiber.Map{"error": "invalid response type"})
+		return c.Render("internal_error", fiber.Map{
+			"EntryPoint":  c.OriginalURL(),
+			"ErrorCode":   "ERR1002",
+			"RedirectURI": authRequest.RedirectURI,
+		})
 	}
 	if !strings.Contains(authRequest.RedirectURI, "https://") {
-		return c.Status(400).JSON(fiber.Map{"error": "invalid redirect uri"})
+		return c.Render("internal_error", fiber.Map{
+			"EntryPoint":  c.OriginalURL(),
+			"RedirectURI": authRequest.RedirectURI,
+			"ErrorCode":   "ERR1003",
+		})
 	}
 	if authRequest.ClientID == "" {
-		return c.Status(400).JSON(fiber.Map{"error": "invalid client id"})
+		return c.Render("internal_error", fiber.Map{
+			"RedirectURI": authRequest.RedirectURI,
+			"EntryPoint":  c.OriginalURL(),
+			"ErrorCode":   "ERR1004",
+		})
 	}
 	if authRequest.Scope == "" {
-		return c.Status(400).JSON(fiber.Map{"error": "invalid scope"})
+		return c.Render("internal_error", fiber.Map{
+			"RedirectURI": authRequest.RedirectURI,
+			"EntryPoint":  c.OriginalURL(),
+			"ErrorCode":   "ERR1005",
+		})
 	}
 	if authRequest.State == "" {
-		return c.Status(400).JSON(fiber.Map{"error": "invalid state"})
+		return c.Render("internal_error", fiber.Map{
+			"RedirectURI": authRequest.RedirectURI,
+			"EntryPoint":  c.OriginalURL(),
+			"ErrorCode":   "ERR1006",
+		})
 	}
 
 	// verify client exists
 	client := new(model.Client)
 	if err := db.Where("name = ?", authRequest.ClientID).First(&client).Error; err != nil {
-		return c.Status(400).JSON(fiber.Map{"error": "unknown client"})
+		return c.Render("internal_error", fiber.Map{
+			"RedirectURI": authRequest.RedirectURI,
+			"EntryPoint":  c.OriginalURL(),
+			"ErrorCode":   "ERR1007",
+		})
 	}
 
 	// generate code
 	code, err := cuid.NewCrypto(rand.Reader)
 	if err != nil {
-		return c.Status(500).JSON(fiber.Map{"error": "internal server error"})
+		return c.Render("internal_error", fiber.Map{
+			"RedirectURI": authRequest.RedirectURI,
+			"EntryPoint":  c.OriginalURL(),
+			"ErrorCode":   "ERR1008",
+		})
 	}
 
 	c.Cookie(&fiber.Cookie{
@@ -78,16 +110,21 @@ func Login(c *fiber.Ctx) error {
 }
 
 func RedirectOrLogin(c *fiber.Ctx) error {
-	// currently, just redirect
 	redirectReq := new(model.RedirectOrLoginRequest)
 	if err := c.QueryParser(redirectReq); err != nil {
-		return c.Status(400).JSON(fiber.Map{"error": "invalid request"})
+		return c.Render("internal_error2", fiber.Map{
+			"ErrorCode": "ERR3000",
+		})
 	}
 	if redirectReq.ClientRedirectURI == "" {
-		return c.Status(400).JSON(fiber.Map{"error": "invalid request"})
+		return c.Render("internal_error2", fiber.Map{
+			"ErrorCode": "ERR3001",
+		})
 	}
 	if redirectReq.EntryPoint == "" {
-		return c.Status(400).JSON(fiber.Map{"error": "invalid request"})
+		return c.Render("internal_error2", fiber.Map{
+			"ErrorCode": "ERR3002",
+		})
 	}
 
 	if redirectReq.RedirectToOrigin {
@@ -100,17 +137,23 @@ func RedirectOrLogin(c *fiber.Ctx) error {
 func ConfirmAuth(c *fiber.Ctx) error {
 	db, err := gorm.Open(postgres.Open(os.Getenv("DATABASE_URL")), &gorm.Config{})
 	if err != nil {
-		panic("unable to open db: " + err.Error())
+		return c.Render("internal_error2", fiber.Map{
+			"ErrorCode": "ERR2000",
+		})
 	}
 	tempCode := c.Cookies("temp_auth_request_code")
 	if tempCode == "" {
-		return c.Status(400).JSON(fiber.Map{"error": "invalid temp auth code"})
+		return c.Render("internal_error2", fiber.Map{
+			"ErrorCode": "ERR2001",
+		})
 	}
 	c.ClearCookie("temp_auth_request_code")
 
 	authConfirmReq := new(model.ConfirmAuthRequest)
 	if err := c.QueryParser(authConfirmReq); err != nil {
-		return c.Status(400).JSON(fiber.Map{"error": "invalid request"})
+		return c.Render("internal_error2", fiber.Map{
+			"ErrorCode": "ERR2002",
+		})
 	}
 
 	if authConfirmReq.Identity == "" || authConfirmReq.ClientID == "" || authConfirmReq.Password == "" {
@@ -124,7 +167,9 @@ func ConfirmAuth(c *fiber.Ctx) error {
 	// verify client exists
 	client := new(model.Client)
 	if err := db.Where("name = ?", authConfirmReq.ClientID).First(&client).Error; err != nil {
-		return c.Status(404).JSON(fiber.Map{"error": "unknown client"})
+		return c.Render("internal_error2", fiber.Map{
+			"ErrorCode": "ERR2003",
+		})
 	}
 
 	// redirect with access denied
@@ -136,7 +181,9 @@ func ConfirmAuth(c *fiber.Ctx) error {
 	user := new(model.User)
 	if err := db.Where("email = ?", authConfirmReq.Identity).First(&user).Error; err != nil {
 		println(err.Error)
-		return c.Status(404).JSON(fiber.Map{"error": "unkown user"})
+		return c.Render("internal_error2", fiber.Map{
+			"ErrorCode": "ERR2004",
+		})
 	}
 
 	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(authConfirmReq.Password)); err != nil {
